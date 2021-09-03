2911dc8d9966ea274a1b244e43bde2e9d4aa2294
erodrigufer <erodrigufer@gmail.com>
2021-09-03 10:41:29 +0200

    finish compile script

--- compile.sh
+++ compile.sh
@@ -11,7 +11,23 @@ USER_INPUT=$1
 # and not an alias of ls like "ls -1"
 FILES_TO_COMPILE=$("ls" *.c)
 
-echo $FILES_TO_COMPILE
+compile_with_standard_name() {
 
-[ -z "$USER_INPUT" ] && echo "User input empty!"
-[ -n "$USER_INPUT" ] && echo "User input not-empty!"
+	gcc -Wall "$FILES_TO_COMPILE"
+
+}
+
+compile_with_custom_name() {
+
+	gcc -Wall -o "$USER_INPUT" "$FILES_TO_COMPILE"
+
+}
+
+# User input is empty, then compile with standard name
+[ -z "$USER_INPUT" ] && compile_with_standard_name
+
+# User input is not empty, then compile with custom (user input) name
+[ -n "$USER_INPUT" ] && compile_with_custom_name
+
+
+exit 0
