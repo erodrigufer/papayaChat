4bc62e1f3678e842c1873c5e5ef6e9f51000825b
erodrigufer <erodrigufer@gmail.com>
2021-09-03 11:01:15 +0200

    fix bug with *.c multiple c files

--- compile_c.sh
+++ compile_c.sh
@@ -5,27 +5,15 @@
 # user input can also be none, then use standard file output name
 USER_INPUT=$1
 
-# This variable lists all files that will be compiled together, which are
-# all C-files in this directory
-# The ls programm is written in parenthesis, so that exactly ls is executed
-# and not an alias of ls like "ls -1"
-FILES_TO_COMPILE=$(ls -C prueba.c)
-
-# TO DO: change FILES_TO_COMPILE with awk or something, so that each file name gets its PWD
-
-echo $PWD
-
-cd $PWD
-
 compile_with_standard_name() {
 
-	gcc -Wall "${PWD}/${FILES_TO_COMPILE}"
+	gcc -Wall *.c
 
 }
 
 compile_with_custom_name() {
 
-	gcc -Wall -o "$USER_INPUT" "$FILES_TO_COMPILE"
+	gcc -Wall -o "$USER_INPUT" *.c
 
 }
 
