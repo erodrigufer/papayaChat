5d4f785ebef672d0dac03728ec44152f13ce3c2f
erodrigufer <erodrigufer@gmail.com>
2021-09-03 10:37:33 +0200

    add test methods to detect user input

--- compile.sh
+++ compile.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# compile_c.sh
+
+# use 1st parameter of bash script as name for compiled file
+# user input can also be none, then use standard file output name
+USER_INPUT=$1
 
 # This variable lists all files that will be compiled together, which are
 # all C-files in this directory
@@ -8,3 +13,5 @@ FILES_TO_COMPILE=$("ls" *.c)
 
 echo $FILES_TO_COMPILE
 
+[ -z "$USER_INPUT" ] && echo "User input empty!"
+[ -n "$USER_INPUT" ] && echo "User input not-empty!"
