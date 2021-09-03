bb4e2f620d93717e981379b917088ace050e3390
erodrigufer <erodrigufer@gmail.com>
2021-09-03 09:57:18 +0200

    change name of daemon macro flags

--- daemonCreation.h
+++ daemonCreation.h
@@ -10,13 +10,13 @@ This are self-defined macros (or flag values) to be used as inputs with the 'fla
 for the function daemonCreation()
 */
 
-#define BD_NO_CHDIR           01    /* Don't chdir("/") */
-#define BD_NO_CLOSE_FILES     02    /* Don't close all open files */
-#define BD_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
+#define DAEMON_FLAG_NO_CHDIR           01    /* Don't chdir("/") */
+#define DAEMON_FLAG_NO_CLOSE_FILES     02    /* Don't close all open files */
+#define DAEMON_FLAG_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
                                        stderr to /dev/null */
-#define BD_NO_UMASK0         010    /* Don't do a umask(0) */
+#define DAEMON_FLAG_NO_UMASK0         010    /* Don't do a umask(0) */
 
-#define BD_MAX_CLOSE  8192          /* Maximum file descriptors to close if
+#define DAEMON_FLAG_MAX_CLOSE  8192          /* Maximum file descriptors to close if
                                        sysconf(_SC_OPEN_MAX) is indeterminate */
 
 int daemonCreation(int flags);
