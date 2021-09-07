a351fee65871af16a9a2bb1b32db379ffec7177c
Hap <erodrigufer@gmail.com>
2021-09-07 11:13:13 +0000

    at getpid() func, style of license corrected

--- main.c
+++ main.c
@@ -4,6 +4,18 @@
 /* add sys logging capabilities to daemon */
 #include <syslog.h>
 
+/* libraries needed to prnt the pid of a process,
+both libraries are already included in "basics.h" */
+//#include <sys/types.h>
+//#include <unistd.h>
+
+/*
+This is how the getpid() function is declared,
+this functions always work (see documentation man 2 getpid [getpid is a kernel system call
+so it is found on (2)])
+       pid_t getpid(void);
+       pid_t getppid(void);
+*/
 int configureSysLog(void){
 
 	/* identityString: this const char* will be appended to all log messages */
@@ -31,6 +43,16 @@ int main(int argc, char *argv[]){
 		exit(EXIT_FAILURE); /* at this point stderr was probably redirected to 
 		/dev/null so there is no point on writing an error message */
 
+
+/*
+int snprintf(char *str, size_t size, const char *format, ...);
+Function definition to 
+*/
+
+	pid_t daemonpid = getpid(); /* declare daemonpid to print on syslog */
+
+	/* cast pid_t to a long inside the snprintf call, since there is no specifier for pid_t */ 
+		
 	syslog(LOG_INFO, "Test #1");
 
 	closelog();
@@ -40,4 +62,4 @@ int main(int argc, char *argv[]){
 
 
 
-/* Eduardo Rodriguez 2021 (c) (@erodrigufer) with some code taken and modified from Michael Kerrisk GNU GPLv3 */
+/* Eduardo Rodriguez 2021 (c) (@erodrigufer) with some code taken and modified from Michael Kerrisk. Licensed under GNU GPLv3 */
