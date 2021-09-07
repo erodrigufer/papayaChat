09e47474783f07cc09b61ed5e78b9b3bba8bd2df
erodrigufer <erodrigufer@gmail.com>
2021-09-07 22:00:55 +0200

    add files for concurrent tcp server

---
+++ concurrent_tcp_server.c
@@ -0,0 +1,109 @@
+/* is_echo_sv.c
+
+   An implementation of the TCP "echo" service.
+
+   NOTE: this program must be run under a root login, in order to allow the
+   "echo" port (7) to be bound. Alternatively, for test purposes, you can
+   replace the SERVICE name below with a suitable unreserved port number
+   (e.g., "51000"), and make a corresponding change in the client.
+
+   See also is_echo_cl.c.
+*/
+#include <signal.h>
+#include <syslog.h>
+#include <sys/wait.h>
+#include "daemonCreation.h"
+#include "inet_sockets.h"       /* Declarations of inet*() socket functions */
+#include "basics.h"
+
+#define SERVICE "echo"          /* Name of TCP service */
+#define BUF_SIZE 4096
+
+static void             /* SIGCHLD handler to reap dead child processes */
+grimReaper(int sig)
+{
+    int savedErrno;             /* Save 'errno' in case changed here */
+
+    savedErrno = errno;
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        continue;
+    errno = savedErrno;
+}
+
+/* Handle a client request: copy socket input back to socket */
+
+static void
+handleRequest(int client_fd)
+{
+    char buf[BUF_SIZE];
+    ssize_t numRead;
+
+    while ((numRead = read(client_fd, buf, BUF_SIZE)) > 0) {
+        if (write(client_fd, buf, numRead) != numRead) {
+            syslog(LOG_ERR, "write() failed: %s", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (numRead == -1) {
+        syslog(LOG_ERR, "Error from read(): %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+}
+
+int
+main(int argc, char *argv[])
+{
+    int listen_fd, client_fd;               /* Listening and connected sockets */
+    struct sigaction sa;
+
+    if (daemonCreation(0) == -1)
+        errExit("becomeDaemon");
+
+    /* Establish SIGCHLD handler to reap terminated child processes */
+
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = SA_RESTART;
+		/* grimReaper is the function handler for a SIGCHLD signal */
+    sa.sa_handler = grimReaper;
+    if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+        syslog(LOG_ERR, "Error from sigaction(): %s", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    listen_fd = inetListen(SERVICE, 10, NULL);
+    if (listen_fd == -1) {
+				/* The listening socket could not be created.
+				*/
+        syslog(LOG_ERR, "Could not create server socket (%s)", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+
+    for (;;) {
+        client_fd = accept(listen_fd, NULL, NULL);  /* Wait for connection */
+        if (client_fd == -1) {
+            syslog(LOG_ERR, "Failure in accept(): %s", strerror(errno));
+            exit(EXIT_FAILURE);
+        }
+
+        /* Handle each client request in a new child process */
+
+        switch (fork()) {
+        case -1:
+            syslog(LOG_ERR, "Can't create child (%s)", strerror(errno));
+            close(client_fd);                 /* Give up on this client */
+            break;                      /* May be temporary; try next client */
+
+        case 0:                         /* Child */
+            close(listen_fd);                 /* Unneeded copy of listening socket */
+            handleRequest(client_fd);
+            _exit(EXIT_SUCCESS);
+
+        default:                        /* Parent */
+            close(client_fd);                 /* Unneeded copy of connected socket */
+            break;                      /* Loop to accept next connection */
+        }
+    }
+}
+
+/* Eduardo Rodriguez 2021 (c) (@erodrigufer) with some code taken and modified from Michael Kerrisk. Licensed under GNU GPLv3 */
---
+++ inet_sockets.h
@@ -0,0 +1,25 @@
+/* inet_sockets.h
+
+*/
+#ifndef INET_SOCKETS_H
+#define INET_SOCKETS_H          /* Header guard */
+
+#include <sys/socket.h>
+#include <netdb.h>
+
+int inetConnect(const char *host, const char *service, int type);
+
+int inetListen(const char *service, int backlog, socklen_t *addrlen);
+
+int inetBind(const char *service, int type, socklen_t *addrlen);
+
+char *inetAddressStr(const struct sockaddr *addr, socklen_t addrlen,
+                char *addrStr, int addrStrLen);
+
+#define IS_ADDR_STR_LEN 4096
+                        /* Suggested length for string buffer that caller
+                           should pass to inetAddressStr(). Must be greater
+                           than (NI_MAXHOST + NI_MAXSERV + 4) */
+#endif
+
+/* Eduardo Rodriguez 2021 (c) (@erodrigufer) with some code taken and modified from Michael Kerrisk. Licensed under GNU GPLv3 */
