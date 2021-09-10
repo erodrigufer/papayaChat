2917023b8047a652c636750f0332ab340cad9777
erodrigufer <erodrigufer@gmail.com>
2021-09-10 12:05:03 +0200

    change function name inetConnect()

--- inet_sockets.h
+++ inet_sockets.h
@@ -8,11 +8,12 @@
 #include <netdb.h>
 
 
-/* Create a socket with the given socket 'type', and connect it to the
+/* Client-side function:
+Create a socket with the given socket 'type', and connect it to the
 address specified by 'host' and 'service'. It can handle both TCP and 
 UDP clients, that connect their sockets to a server 
 Returns a file descriptor on success, or a -1 on error */
-int inetConnect(const char *host, const char *service, int type);
+int clientConnect(const char *host, const char *service, int type);
 
 int inetListen(const char *service, int backlog, socklen_t *addrlen);
 
