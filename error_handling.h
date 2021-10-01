9622fe51e80746912e6c2c313f07570adbb69b0b
erodrigufer <erodrigufer@gmail.com>
2021-10-01 16:58:36 +0200

    comment GCC macro definition and attributes

--- error_handling.h
+++ error_handling.h
@@ -8,17 +8,25 @@
 
 void errMsg(const char *format, ...);
 
+/* only if the compiler is gcc, the following macros will be defined
+In other words, one can use GNU extensions/attributes */
 #ifdef __GNUC__
 
     /* This macro stops 'gcc -Wall' complaining that "control reaches
        end of non-void function" if we use the following functions to
        terminate main() or some other non-void function. */
 
+/* compiler optimization attribute
+check for more info: 
+https://gcc.gnu.org/onlinedocs/gcc-3.2/gcc/Function-Attributes.html */
 #define NORETURN __attribute__ ((__noreturn__))
 #else
-#define NORETURN
+#define NORETURN /* if we are not using GCC then NORETURN will just be
+a blank macro definition */
 #endif
 
+/* Display error message including 'errno' diagnostic, and
+   terminate the process */
 void errExit(const char *format, ...) NORETURN ;
 
 void err_exit(const char *format, ...) NORETURN ;
