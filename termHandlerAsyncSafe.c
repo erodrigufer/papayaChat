c0da1c1a6d3e87cc799f8764cebad97a34168d60
Hap <erodrigufer@gmail.com>
2021-10-15 11:32:45 +0000

    remove return 0, unnecessary

--- termHandlerAsyncSafe.c
+++ termHandlerAsyncSafe.c
@@ -1,16 +1,16 @@
 #include <syslog.h>				/* server runs as a daemon, logging handled
 								through syslog API */
+#include "configure_syslog.h" /* configure syslog to be able to add log at termination */
 #include "basics.h"
 
 int main(int argc, char *argv[]){
+
+	configure_syslog();
 	
 	/* SIGTERM is the default signal sent to a process when the 'kill' command is used 
 	in the terminal (when no other signal is specified). Use this signal to kill the 
 	parent/listening server! */
 	syslog(LOG_DEBUG, "SIGTERM signal received. Killing process!");
 	exit(EXIT_SUCCESS);
-	
-	/* this point should never be achieved, the return is added to avoid compiler
-	messages */
-	return 0;
+
 }
