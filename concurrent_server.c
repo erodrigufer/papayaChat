68642523a2a0653f91c4023bca1bea4c1760ff70
erodrigufer <erodrigufer@gmail.com>
2021-10-15 17:09:11 +0200

    add syslog for child terminating

--- concurrent_server.c
+++ concurrent_server.c
@@ -245,6 +245,7 @@ main(int argc, char *argv[])
 										an input parameter, because it would otherwise not know
 										to which and from which file descriptor to perform
 										write and read calls */
+			syslog(LOG_INFO, "Child process terminated.");
             _exit(EXIT_SUCCESS);		/* child processes should generally only call _exit();
 										for a more general discussion about the topic check
 										25.4 'The Linux Programming Interface' */
