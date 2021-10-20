07fff46f64b9cba57bb3cece6a4808626fcc3b45
erodrigufer <erodrigufer@gmail.com>
2021-10-20 13:08:18 +0200

    add message explaining never ending read call

--- client.c
+++ client.c
@@ -31,6 +31,10 @@ handleReadSocket(int server_fd){
 	ssize_t bytesRead;
 	char string_buf[BUF_SIZE];
 
+	/* infinte for-loop reading from the TCP-socket
+	every time a whole message is read, then read() returns 0
+	and the while-loop will then re-start with a blocking read()
+	until some bytes can be read from the socket */
 	for(;;){
 		while((bytesRead = read(server_fd, string_buf, BUF_SIZE)) > 0){
 			printf("%s",string_buf); /* later implement this with write
