26a6255d28012138667e408d998617a33b13edec
erodrigufer <erodrigufer@gmail.com>
2021-11-02 19:41:21 +0100

    change to buggy function to test with strace

--- frontEnd.c
+++ frontEnd.c
@@ -283,7 +283,7 @@ main(int argc, char *argv[])
 			close(pipe_fds_receive_server[0]);
 			/* Close pipe from 1. Child inherited through parent */
 			close(pipe_fds_send_server[1]);
-			handleReadSocket(server_fd, pipe_fds_receive_server[1]);
+			handleReadSocket2(server_fd, pipe_fds_receive_server[1]);
 			_exit(EXIT_SUCCESS);
 			break;
 
