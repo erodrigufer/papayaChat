0cd78d3a3d88995f98c8c4e1e060d0d71cdbd44f
erodrigufer <erodrigufer@gmail.com>
2021-11-09 11:22:29 +0100

    add header guard

--- CONFIG.h
+++ CONFIG.h
@@ -1,4 +1,11 @@
+/* File defines IP and port of chat service server */
+
+#ifndef CONFIG_H /* add header guard */
+#define CONFIG_H
+
 /* Define TCP/IP services */
 #define SERVICE "51000" /* Port to connect to with server */
 //#define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
 #define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
+
+#endif
