3fec6537801b84fa6c27c7f622d3393a084d018b
erodrigufer <erodrigufer@gmail.com>
2021-11-09 11:20:32 +0100

    add config file to input services in a more straight forward fashion

---
+++ CONFIG.h
@@ -0,0 +1,4 @@
+/* Define TCP/IP services */
+#define SERVICE "51000" /* Port to connect to with server */
+//#define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
+#define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
