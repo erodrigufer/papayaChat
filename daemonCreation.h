df636826a4e8140fb6e8a9e72bd5a2f46431089d
erodrigufer <erodrigufer@gmail.com>
2021-11-22 17:29:59 +0100

    [minor] fix indenting

--- daemonCreation.h
+++ daemonCreation.h
@@ -13,7 +13,7 @@ for the function daemonCreation()
 #define DAEMON_FLAG_NO_CHDIR           01    /* Don't chdir("/"), do not change the working directory to '/' */
 #define DAEMON_FLAG_NO_CLOSE_FILES     02    /* Don't close all open files */
 #define DAEMON_FLAG_NO_REOPEN_STD_FDS  04    /* Don't reopen stdin, stdout, and
-                                       stderr to /dev/null */
+                                       		 stderr to /dev/null */
 #define DAEMON_FLAG_NO_UMASK0         010    /* Don't do a umask(0) */
 
 #define DAEMON_FLAG_MAX_CLOSE  8192          /* Maximum file descriptors to close if
