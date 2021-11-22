e420b66f3308bbee60b389e9b6e117958e9acd7e
erodrigufer <erodrigufer@gmail.com>
2021-11-22 16:42:13 +0100

    remove comment describing program

--- concurrent_server.c
+++ concurrent_server.c
@@ -1,12 +1,5 @@
 /* concurrent_tcp_server.c
 
-   An implementation of the TCP "echo" service.
-
-   NOTE: this program must be run under a root login, in order to allow the
-   "echo" port (7) to be bound. Alternatively, for test purposes, you can
-   replace the SERVICE name below with a suitable unreserved port number
-   (e.g., "51000"), and make a corresponding change in the client.
-
 */
 #include <signal.h>				/* check 'man 2 sigaction' signal.h is needed
 								to change the disposition of signals with
