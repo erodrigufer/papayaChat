cc06450fc13dc913be165b605a8dcff20df32ede
erodrigufer <erodrigufer@gmail.com>
2021-11-22 17:08:59 +0100

    create function to open/create chat log file

--- CONFIG.h
+++ CONFIG.h
@@ -4,16 +4,20 @@ File defines IP and port of chat service server
 SERVICE is the PORT number
 HOST is the server's IP
 
+It also defines the path where to store the central chat log file
+CHAT_LOG_PATH
 */
 
 #ifndef CONFIG_H /* add header guard */
 #define CONFIG_H
 
 /* Define TCP/IP services */
-//#define SERVICE "51000" /* Port to connect to with server */
-#define SERVICE "50000" /* Port to connect to with server */
+#define SERVICE "51000" /* Port to connect to with server */
+//#define SERVICE "50000" /* Port to connect to with server */
 //#define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
-#define HOST "payaserver" /* server address (found, e.g. on /etc/hosts file) */
-//#define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
+//#define HOST "payaserver" /* server address (found, e.g. on /etc/hosts file) */
+#define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
 
+/* Define path of chat log file */
+#define CHAT_LOG_PATH "./chat_log"
 #endif
---
+++ file_locking.c
@@ -0,0 +1,43 @@
+/* file_locking.c
+
+Use these functions to avoid race conditions, when writing or reading with multiple
+processes from a single file.
+
+The server back-end creates a child process for each client being served. These child processes
+can asynchronously read and write to a central unique chat log file. Therefore, it is 
+imperative to avoid race conditions when handling that chat log file. 
+
+*/
+
+/* Required for open(2) 
+(next three headers) */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/* CONFIG.h header file includes the path where the central chat log file
+will be stored; defined under CHAT_LOG_PATH as a string */
+#include "CONFIG.h"
+
+/* open the central chat log file
+If file does not exist, it creates the file.
+It returns fd of file if file is created or opened correctly.
+If not, it returns -1 */
+int
+openChatLogFile(void)
+{
+
+	/* Define flags for open(2)
+	-open for READ/WRITE
+	-if file does not exist, CREATE file
+	-when writting, always APPEND to file (atomic call)
+	-CLOSE file descriptor on EXEC 
+	*/
+	int flags = O_RDWR | O_CREAT | O_APPEND | O_CLOEXEC ;
+
+	/* File permissions (when file is created) */
+	mode_t createPermissions = S_IRUSR | S_IWUSR;
+
+	return open(CHAT_LOG_PATH,flags,createPermissions);
+
+}
