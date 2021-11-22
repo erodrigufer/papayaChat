3dca6b0ffc03d3567e51d5fe3ed769bc6413d995
erodrigufer <erodrigufer@gmail.com>
2021-11-22 17:28:15 +0100

    create header file for file_locking.h

---
+++ file_locking.h
@@ -0,0 +1,10 @@
+/* file_locking.h  */
+
+#ifndef FILE_LOCKING_H 		/* header guard */
+#define FILE_LOCKING_H
+
+/* open (or if non-existent, create) central chat log file */
+int openChatLogFile(void);
+
+#endif
+/* Eduardo Rodriguez 2021 (c) (@erodrigufer). Licensed under GNU AGPLv3 */
