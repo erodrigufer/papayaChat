765c88b69d14144c3ff7e24f860799f516592722
erodrigufer <erodrigufer@gmail.com>
2021-11-23 20:18:06 +0100

    add handleMessages header

---
+++ handleMessages.h
@@ -0,0 +1,11 @@
+/* handleMessages.h
+
+*/
+
+#ifndef HANDLEMESSAGES_H /* header guard */
+#define HANDLEMESSAGES_H
+
+
+#endif
+
+/* Eduardo Rodriguez 2021 (c) (@erodrigufer). Licensed under GNU AGPLv3 */
