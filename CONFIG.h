211517f62a41704d0764035cd678c745270920e0
erodrigufer <erodrigufer@gmail.com>
2021-11-23 12:30:01 +0100

    [minor] comment styling

--- CONFIG.h
+++ CONFIG.h
@@ -1,10 +1,12 @@
 /* CONFIG.h
 
-File defines IP and port of chat service server 
-SERVICE is the PORT number
-HOST is the server's IP
+Default config file
 
-It also defines the path where to store the central chat log file
+This file defines IP and port of chat service server 
+- SERVICE is the PORT number
+- HOST is the server's IP
+
+It also defines the path to store the central chat log file
 CHAT_LOG_PATH
 */
 
@@ -13,10 +15,9 @@ CHAT_LOG_PATH
 
 /* Define TCP/IP services */
 #define SERVICE "51000" /* Port to connect to with server */
-//#define SERVICE "50000" /* Port to connect to with server */
-//#define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
+#define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
 //#define HOST "payaserver" /* server address (found, e.g. on /etc/hosts file) */
-#define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
+//#define HOST "kah" /* server address (found, e.g. on /etc/hosts file) */
 
 /* Define path of chat log file */
 #define CHAT_LOG_PATH "./chat_log"
