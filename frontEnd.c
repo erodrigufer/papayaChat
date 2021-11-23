34c0f995dd39a880718f80c503756b5ee51610bb
erodrigufer <erodrigufer@gmail.com>
2021-11-23 17:59:34 +0100

    remove global variables required for atexit() Great move!

--- frontEnd.c
+++ frontEnd.c
@@ -15,51 +15,6 @@
 
 #define MAX_MESSAGE_SENT COLS/2 /* max message size of message to be sent */
 
-/* Define this values as global to be able to call them from the atexit() function */
-int child1_pid;
-int child2_pid;
-
-/* run this function to catch SIGCHLD of child processes exiting */
-static void
-catchSIGCHLD(int sig)
-{
-	int savedErrno;             /* Save 'errno' in case changed here, errno
-									can be changed by waitpid() if no more children exist
-									then 'errno'= ECHILD */
-		savedErrno = errno;
-		while (waitpid(-1, NULL, WNOHANG) > 0)	/* 'man 2 waitpid' -1 means that it waits 
-		for any of its child processes, the option WNOHANG makes the syscall waitpid()
-		to return inmediately if no children has exited, if children exist but none has changed 
-		status, then waitpid returns 0, so it exits the while-loop (on error, it returns -1 and
-		also exits the while-loop). In any other case, multiple children have exited, so waitpid()
-		will return their pids (which are larger than 0). After the last children which has changed
-		status, waitpid will return either 0 or -1 and the while-loop will come to an end
-		If there are no more children, then waitpid will return -1 and set errno to ECHILD */
-			continue;
-		errno = savedErrno;			/* restore errno to value before signal handler */
-
-}
-
-/* run this function to kill child processes */
-static void
-killChildProcesses(void)
-{
-	/* IMPORTANT: I should not use errExit with these functions
-	since they are already being run atexit(), so only display 
-	diagnostic error messages */
-	/* kill 1. Child */
-	if(kill(child1_pid,SIGTERM)==-1)
-		errMsg("Failed to kill 1. Child!");
-	
-	/* kill 2. Child */
-	if(kill(child2_pid,SIGTERM)==-1)
-		errMsg("Failed to kill 2. Child!");
-
-/* TODO: debugging purpouses, remove fprintf later */
-	fprintf(stderr,"Successfully killed child processes!\n");
-
-}
-
 /* establish TCP connection with server, return fd of server socket */
 static int 
 establishConnection(void){
