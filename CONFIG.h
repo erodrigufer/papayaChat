6338bab35a3730b83ce43556476beb3a58c27c06
erodrigufer <erodrigufer@gmail.com>
2021-11-23 12:48:59 +0100

    accept user-defined file as well

--- CONFIG.h
+++ CONFIG.h
@@ -13,10 +13,29 @@ CHAT_LOG_PATH
 #ifndef CONFIG_H /* add header guard */
 #define CONFIG_H
 
+/* if personalized specific user-defined config file
+exists include it */
+#include "userConfig.h"
+
+/* if userConfig.h exists, then USERCONFIG_H will be
+defined, and the user-defined macros will be used, not the default
+ones from CONFIG.h */
+
+/* ------------------------------------------------------------------------ */
+#ifndef USERCONFIG_H
+/* IMPORTANT:
+These are the values, that are also represented in userConfig.h if the 
+file exists, append any other values only present in the general (default)
+config file outside this #ifndef part 
+*/
+
 /* Define TCP/IP services */
 #define SERVICE "51000" /* Port to connect to with server */
 #define HOST "localhost" /* server address (found, e.g. on /etc/hosts file) */
 
 /* Define path of chat log file */
 #define CHAT_LOG_PATH "./chat_log"
-#endif
+#endif /* endif for USERCONFIG_H */
+/* ------------------------------------------------------------------------ */
+
+#endif /* endif for header guard */
