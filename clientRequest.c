96465769702adbab22b02a9a487c27d62178d3e8
erodrigufer <erodrigufer@gmail.com>
2021-11-25 15:42:43 +0100

    enable syslog

--- clientRequest.c
+++ clientRequest.c
@@ -1,7 +1,20 @@
+#include <syslog.h>		/* server runs as daemon, pipe errors messages to syslog */
+/* TODO: check if daemon posts still with the configuration of 
+concurrent_server.c, or if I have to add the configure_syslog.h functions */
 #include "clientRequest.h"
 #include "basics.h"
 #include "file_locking.h"
 #include "CONFIG.h"	/* declaration of BUF_SIZE */
+
+/* define greetingMessage string, the compiler allocates enough memory for the string */
+const char greetingMessage [] = "\
+\n\
+                  ...papayaChat...\n\n\
+papayaChat is licensed under GNU AGPLv3.\n\
+The code is hosted at: www.github.com/erodrigufer/papayaChat\n\
+--------------------------------------------------------------\n\
+\n\
+";
 
 /* Handle a client request: copy socket input back to socket,
 good coding practices, function is static, because it should only
@@ -12,17 +25,6 @@ handleRequest(int client_fd)
     char buf[BUF_SIZE];
     ssize_t numRead;
 
-	/* define greetingMessage string, the compiler allocates enough memory for the string */
-	const char greetingMessage [] = "\
-\n\
---------------------------------------------------------------\n\
-                  ...PapayaChat Service...\n\n\
-PapayaChat is licensed under GNU AGPLv3.\n\
-The code is hosted at: www.github.com/erodrigufer/papayaChat\n\
---------------------------------------------------------------\n\
-\n\
-";
-
 	/* the size of the string is calculated statically at compile time,
 	check 'Effective C' page 133 */
 	size_t greetingSize = sizeof greetingMessage;
@@ -30,21 +32,21 @@ The code is hosted at: www.github.com/erodrigufer/papayaChat\n\
 	/* the write() call should write exactly greetingSize bytes, otherwise
 	it has failed */
 	if(write(client_fd,greetingMessage,greetingSize)!=greetingSize){
-		//syslog(LOG_ERR, "write() failed: %s", strerror(errno));
+		syslog(LOG_ERR, "write() failed: %s", strerror(errno));
 		_exit(EXIT_FAILURE);
 	}
     
 	while ((numRead = read(client_fd, buf, BUF_SIZE)) > 0) {
         if (write(client_fd, buf, numRead) != numRead) {
-            //syslog(LOG_ERR, "write() failed: %s", strerror(errno));
+            syslog(LOG_ERR, "write() failed: %s", strerror(errno));
             _exit(EXIT_FAILURE);
         }
 		/* add debug syslog to see amount of bytes received from client */
-		//syslog(LOG_DEBUG, "%ld Bytes received from client.", numRead);
+		syslog(LOG_DEBUG, "%ld Bytes received from client.", numRead);
     }
 
     if (numRead == -1) {
-        //syslog(LOG_ERR, "Error from read(): %s", strerror(errno));
+        syslog(LOG_ERR, "Error from read(): %s", strerror(errno));
         _exit(EXIT_FAILURE);
     }
 
