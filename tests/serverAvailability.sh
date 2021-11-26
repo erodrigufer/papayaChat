3dd69b897e654ec8ce15ca679e6a1478f4a7ab44
Hap <erodrigufer@gmail.com>
2021-11-26 13:47:57 +0000

    add test for ports being in use

--- tests/serverAvailability.sh
+++ tests/serverAvailability.sh
@@ -4,15 +4,16 @@ COLOR_GREEN='\e[0;32m'
 NO_COLOR='\033[0m'
 COLOR_RED='\033[0;31m'
 
-echo "[test] Test server availability..."
-# Install dependencies
-# ss (instead of netstat)
-echo "Check dependencies..."
-which ss || { echo "ss is missing"; printf "[${COLOR_RED}MISSING DEPENDENCY${NO_COLOR}] Server availability test failed!\n"; exit -1; }
-
 PORT=51000
 # sleep after starting daemon, because daemon needs some time to be up and running
 SLEEP_TIME=2 # in seconds
+
+echo "[test] Test server availability..."
+echo "Check dependencies..."
+# ss (instead of netstat)
+which ss || { echo "ss is missing"; printf "[${COLOR_RED}MISSING DEPENDENCY${NO_COLOR}] Server availability test failed!\n"; exit -1; }
+# Check if PORT is already in use
+ss -at | grep ${PORT} && { printf "[${COLOR_RED}FAILED${NO_COLOR}] Port ${PORT} already in use. A server instance is probably already running. Exit test!"; exit -1; }
 
 # netcat -zv Verbose output -z check for connection
 ../bin/concurrent_server.bin && { echo "Starting server..."; sleep ${SLEEP_TIME}; echo "Server daemon is now running..."; ss -at | grep ${PORT}; } && netcat -zv localhost ${PORT} || { printf "[${COLOR_RED}FAILED${NO_COLOR}] Server availability test failed!\n"; exit -1; }
