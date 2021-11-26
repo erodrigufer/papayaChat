3ecbba723774e7fabe3235f17573efea2de1216c
erodrigufer <erodrigufer@gmail.com>
2021-11-26 13:50:32 +0100

    more output and style for test

--- tests/serverAvailability.sh
+++ tests/serverAvailability.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo "[test] Test server availability..."
 # Install dependencies
 # ss (instead of netstat)
 echo "Check dependencies..."
@@ -10,11 +11,11 @@ PORT=51000
 SLEEP_TIME=3 # in seconds
 
 # netcat -zv Verbose output -z check for connection
-../bin/concurrent_server.bin && { echo "Starting server..."; sleep ${SLEEP_TIME}; echo "Server daemon running."; ss -at | grep ${PORT}; } && netcat -zv localhost ${PORT} || { echo "Server availability failed!"; exit -1; }
+../bin/concurrent_server.bin && { echo "Starting server..."; sleep ${SLEEP_TIME}; echo "Server daemon is now running..."; ss -at | grep ${PORT}; } && netcat -zv localhost ${PORT} || { echo "[FAILED] Server availability test failed!"; exit -1; }
 
-kill $(pidof concurrent_server.bin) && echo "Killed daemon"
+kill $(pidof concurrent_server.bin) && echo "Killed daemon..."
 # ss -a (listening and active ports) -t (TCP connections)
 ss -at | grep ${PORT}
-echo "Connection test passed!"
+echo "[SUCCESS] Server availability test passed!"
 exit 0
 
