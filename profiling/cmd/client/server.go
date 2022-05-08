9a639d2c0a0274a3714fbaf7c5b6971d54befd8d
erodrigufer <erodrigufer@gmail.com>
2022-05-08 16:00:14 +0200

    add proper logging to server

--- profiling/cmd/client/server.go
+++ profiling/cmd/client/server.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 )
 
 // configValues, parsed from the flags
@@ -14,35 +15,50 @@ type configValues struct {
 	addr string
 }
 
+type application struct {
+	errorLog *log.Logger // error log handler
+	infoLog  *log.Logger // info log handler
+}
+
 func main() {
 
 	// Define default HOST and PORT, in case flag is not present
 	DEFAULT_SERVICE := ":3333"
 
 	cfg := new(configValues)
+	app := new(application) // Create a logger for INFO messages, the prefix "INFO" and a tab will be
+	// displayed before each log message. The flags Ldate and Ltime provide the
+	// local date and time
+	app.infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create an ERROR messages logger, addiotionally use the Lshortfile flag to
+	// display the file's name and line number for the error
+	app.errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	flag.StringVar(&cfg.addr, "addr", DEFAULT_SERVICE, "Server's listening address")
 
+	app.infoLog.Printf("Server listening on %s", cfg.addr)
 	ln, err := net.Listen("tcp", cfg.addr)
 	defer ln.Close()
 	if err != nil {
-		log.Println(err)
+		app.errorLog.Println(err)
 	}
 
 	// infinite for-loop, accept clients and create goroutine to handle client
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Println(err)
+			app.errorLog.Println(err)
 		}
-		go handleConnection(conn)
+		go app.handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func (app *application) handleConnection(conn net.Conn) {
 	// read the data up to the newline into netData
 	netData, err := bufio.NewReader(conn).ReadString('\n')
 	if err != nil {
-		log.Println(err)
+		app.errorLog.Println(err)
 		return
 	}
 	fmt.Println(netData)
