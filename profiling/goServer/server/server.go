8a3dd658eff1692403a4da0b084a3618e7c4fb04
erodrigufer <erodrigufer@gmail.com>
2022-05-14 21:06:18 +0200

    fix formatting of print statements

--- profiling/goServer/server/server.go
+++ profiling/goServer/server/server.go
@@ -71,11 +71,11 @@ func (app *application) handleConnection(conn net.Conn) {
 
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Printf("Error occured: %q", err)
+			fmt.Printf("Error occured: %q\n", err)
 			break
 		}
-		fmt.Printf("Bytes received: %d", n)
-		fmt.Printf("%q", buf[:n])
+		fmt.Printf("Bytes received: %d\n", n)
+		fmt.Printf("%s\n", buf[:n])
 
 	}
 }
