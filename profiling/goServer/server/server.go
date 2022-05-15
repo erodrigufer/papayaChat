15fccec693749942c9221a8afaba3be5427f665a
erodrigufer <erodrigufer@gmail.com>
2022-05-15 18:18:50 +0200

    Revert "simply program to possibly improve performance"
    This reverts commit 652dbfb7b7c4598ce46fa007e1f8ea8967987719.

--- profiling/goServer/server/server.go
+++ profiling/goServer/server/server.go
@@ -1,33 +1,59 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net"
+	"os"
 )
+
+// configValues, parsed from the flags
+type configValues struct {
+	// addr, defines the port on which the server will be listening
+	addr string
+}
+
+type application struct {
+	errorLog *log.Logger // error log handler
+	infoLog  *log.Logger // info log handler
+}
 
 func main() {
 
 	// Define default HOST and PORT, in case flag is not present
 	DEFAULT_SERVICE := ":50000"
 
-	fmt.Printf("Server listening on %s\n", DEFAULT_SERVICE)
-	ln, err := net.Listen("tcp", DEFAULT_SERVICE)
+	cfg := new(configValues)
+	app := new(application) // Create a logger for INFO messages, the prefix "INFO" and a tab will be
+	// displayed before each log message. The flags Ldate and Ltime provide the
+	// local date and time
+	app.infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	// Create an ERROR messages logger, addiotionally use the Lshortfile flag to
+	// display the file's name and line number for the error
+	app.errorLog = log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	flag.StringVar(&cfg.addr, "addr", DEFAULT_SERVICE, "Server's listening address")
+
+	app.infoLog.Printf("Server listening on %s\n", cfg.addr)
+	ln, err := net.Listen("tcp", cfg.addr)
 	defer ln.Close()
 	if err != nil {
-		fmt.Println(err)
+		app.errorLog.Println(err)
 	}
 
 	// infinite for-loop, accept clients and create goroutine to handle client
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			fmt.Println(err)
+			app.errorLog.Println(err)
 		}
-		go handleConnection(conn)
+		go app.handleConnection(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func (app *application) handleConnection(conn net.Conn) {
 	defer conn.Close()
 	buf := make([]byte, 1024)
 	// read the data up to the newline into netData
