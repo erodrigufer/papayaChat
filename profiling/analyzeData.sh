d89381aad82a433744c7b03648ac24eb91acd27b
erodrigufer <erodrigufer@gmail.com>
2022-05-18 17:43:17 +0200

    calculate mean of clean data

---
+++ profiling/analyzeData.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Calculate mean value and standard deviation of 
+# every measurement
+analyzeData(){
+	# The 1st parameter of the function is the file that should be processed.
+	local INPUT_FILE=$1
+
+	# The 2nd parameter of the function is the file where the processed data
+	# should be stored.
+	local OUTPUT_FILE=$2
+
+	awk 'BEGIN {mean=0; count=0} {mean+=$1;count+=1} END {print mean/count}' ${INPUT_FILE} 
+
+}
+
+CLEANDATA_PATH="./measurements/15_05_2022/cleanData"
+
+cd ${CLEANDATA_PATH}
+
+FILES=$(ls)
+
+for FILE in $FILES; do
+	# If FILE is a regular file (to avoid iterating over directories).
+	if [ -f ${FILE} ]; then
+		OUTPUT_FILE=../../results
+		analyzeData ${FILE} ${OUTPUT_FILE} && echo "Processed filed: ${FILE}"
+	fi
+done
+
