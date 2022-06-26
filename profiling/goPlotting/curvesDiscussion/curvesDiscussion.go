4256687d4006fe1498e5fca7839389e8c1af5ef2
erodrigufer <erodrigufer@gmail.com>
2022-06-26 20:12:48 +0200

    create figures for mean values and coefficient of variation

--- profiling/goPlotting/curvesDiscussion/curvesDiscussion.go
+++ profiling/goPlotting/curvesDiscussion/curvesDiscussion.go
@@ -13,13 +13,22 @@ func main() {
 	meanC := plotter.XYs{{1, 0.34}, {2, 0.45}, {4, 0.79}, {8, 1.37}, {16, 2.17}, {32, 2.53}, {64, 2.53}}
 	meanGo := plotter.XYs{{1, 1.94}, {2, 2.61}, {4, 4.35}, {8, 6.15}, {16, 11.71}, {32, 22.71}, {64, 36.08}}
 
+	plotValues(meanC, meanGo, "# of concurrent connections", "Mean CPU load in %", "mean.pdf")
+
+	cvC := plotter.XYs{{1, 0.159}, {2, 0.221}, {4, 0.228}, {8, 0.137}, {16, 0.147}, {32, 0.172}, {64, 0.180}}
+	cvGo := plotter.XYs{{1, 0.169}, {2, 0.205}, {4, 0.172}, {8, 0.276}, {16, 0.229}, {32, 0.322}, {64, 0.243}}
+
+	plotValues(cvC, cvGo, "# of concurrent connections", "Coefficient of variation", "cv.pdf")
+}
+
+func plotValues(cValues, goValues plotter.XYs, titleX, titleY, outputFile string) {
 	// Create a new plot, set its title and
 	// axis labels.
 	p := plot.New()
 
 	// p.Title.Text = "Title"
-	p.X.Label.Text = "# of concurrent connections"
-	p.Y.Label.Text = "Mean CPU load in %"
+	p.X.Label.Text = titleX
+	p.Y.Label.Text = titleY
 	// Draw a grid behind the data.
 	p.Add(plotter.NewGrid())
 
@@ -39,8 +48,8 @@ func main() {
 	// 	l.LineStyle.Dashes = []vg.Length{vg.Points(5), vg.Points(5)}
 	// 	l.LineStyle.Color = color.RGBA{B: 255, A: 255}
 
-	// Make a line plotter with points for the meanC measurement.
-	lpLineC, lpPointsC, err := plotter.NewLinePoints(meanC)
+	// Make a line plotter with points for the cValues measurement.
+	lpLineC, lpPointsC, err := plotter.NewLinePoints(cValues)
 	if err != nil {
 		panic(err)
 	}
@@ -49,8 +58,8 @@ func main() {
 	lpPointsC.Shape = draw.PyramidGlyph{}
 	lpPointsC.Color = color.RGBA{R: 255, A: 255}
 
-	// Make a line plotter with points for the meanGo measurement.
-	lpLineGo, lpPointsGo, err := plotter.NewLinePoints(meanGo)
+	// Make a line plotter with points for the goValues measurement.
+	lpLineGo, lpPointsGo, err := plotter.NewLinePoints(goValues)
 	if err != nil {
 		panic(err)
 	}
@@ -68,7 +77,7 @@ func main() {
 	// p.Legend.Add("line points", lpLine, lpPoints)
 
 	// Save the plot to a PDF file.
-	if err := p.Save(4*vg.Inch, 4*vg.Inch, "meanCPU.pdf"); err != nil {
+	if err := p.Save(4*vg.Inch, 4*vg.Inch, outputFile); err != nil {
 		panic(err)
 	}
 }
